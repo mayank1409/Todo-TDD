9166b558ed483cf37a39770ff1d11f2be4d4aab7
Mayank Madhav <mayankmadhav@Mayanks-MacBook-Air.local>
2023-10-24 15:59:07 +0530

    Todo Rest API :: TDD approach

--- mongodb/mongodb.connect.js
+++ mongodb/mongodb.connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     await mongoose.connect(
-      "mongodb+srv://mayankmadhav100:Hello123456@cluster0.dwrqozc.mongodb.net/todos?retryWrites=true",
+      "mongodb+srv://mayankmadhav100:<PASSWORD>@cluster0.dwrqozc.mongodb.net/todos?retryWrites=true",
       { useNewUrlParser: true }
     );
   } catch (err) {
